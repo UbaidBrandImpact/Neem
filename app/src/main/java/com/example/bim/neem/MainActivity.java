9ce2c73af578474bd3e3908174b99a6bfead445f
Samreen <samreen@softsolutions-partner.se>
2017-04-12 11:22:27 +0400

    Home

--- app/src/main/java/com/example/bim/neem/MainActivity.java
+++ app/src/main/java/com/example/bim/neem/MainActivity.java
@@ -1,13 +1,144 @@
 package com.example.bim.neem;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.bim.neem.adapters.DrawerAdapter;
+import com.example.bim.neem.models.DrawerItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private RecyclerView.LayoutManager layoutManager;
+    private DrawerAdapter mDrawerAdapter;
+    private RecyclerView mRecyclerView;
+    private ArrayList<DrawerItem> mDrawerItemList;
+
+    private DrawerLayout mDrawerLayout;
+
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar)findViewById(R.id.appBar);
+
+    //    setSupportActionBar(mToolbar);
+
+
+        mDrawerItemList = new ArrayList<DrawerItem>();
+        DrawerItem item = new DrawerItem();
+
+        item.setTitle("Oral");
+        mDrawerItemList.add(item);
+
+        DrawerItem item2 = new DrawerItem();
+        item2.setTitle("Hair");
+        mDrawerItemList.add(item2);
+
+        DrawerItem item3 = new DrawerItem();
+        item3.setTitle("Skin");
+        mDrawerItemList.add(item3);
+
+        DrawerItem item4 = new DrawerItem();
+        item4.setTitle("Personal");
+        mDrawerItemList.add(item4);
+
+        DrawerItem item5 = new DrawerItem(); //divider
+        item5.setTitle("Divider");
+        mDrawerItemList.add(item5);
+
+        DrawerItem item6 = new DrawerItem();
+        item6.setTitle("Stories");
+        mDrawerItemList.add(item6);
+
+        DrawerItem item7 = new DrawerItem(); //divider
+        item7.setTitle("Divider");
+        mDrawerItemList.add(item7);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.drawerRecyclerView);
+
+        DrawerAdapter adapter = new DrawerAdapter(mDrawerItemList);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(adapter);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open, R.string.close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                //TODO Add some action here
+                //Executed when drawer closes
+
+                Toast.makeText(MainActivity.this, "Closed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                //TODO Add some action here
+                //executes when drawer open
+                Toast.makeText(MainActivity.this, "Opened", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        adapter.setOnItemClickLister(new DrawerAdapter.OnItemSelecteListener() {
+            @Override
+            public void onItemSelected(View v, int position) {
+                Toast.makeText(MainActivity.this, "You clicked at position: "+ position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        try {
+            Glide.with(this).load(R.drawable.products).into((ImageView) findViewById(R.id.products));
+            Glide.with(this).load(R.drawable.videos).into((ImageView) findViewById(R.id.videos));
+            Glide.with(this).load(R.drawable.loyalty_program).into((ImageView) findViewById(R.id.loyaltyprogram));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/bim/neem/adapters/DrawerAdapter.java
@@ -0,0 +1,133 @@
+package com.example.bim.neem.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.example.bim.neem.R;
+import com.example.bim.neem.models.DrawerItem;
+
+import java.util.ArrayList;
+
+/**
+ * Created by darshanz on 7/6/15.
+ */
+public class DrawerAdapter extends RecyclerView.Adapter<DrawerAdapter.DrawerViewHolder> {
+
+    public final static int TYPE_HEADER = 0;
+    public final static int TYPE_MENU = 1;
+    public final static int TYPE_DIVIDER = 2;
+
+
+    private ArrayList<DrawerItem> drawerMenuList;
+
+    private OnItemSelecteListener mListener;
+
+    public DrawerAdapter(ArrayList<DrawerItem> drawerMenuList) {
+        this.drawerMenuList = drawerMenuList;
+    }
+
+    @Override
+    public DrawerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        if(viewType == TYPE_HEADER){
+
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_drawer_header, parent, false);
+
+        }else{
+
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_menu_item, parent, false);
+        }
+
+
+        return new DrawerViewHolder(view, viewType);
+    }
+
+
+    @Override
+    public void onBindViewHolder(DrawerViewHolder holder, int position) {
+        if(position == 0) {
+
+        }
+        else if(position==5 || position ==7)
+        {
+            holder.title.setVisibility(View.GONE);
+            holder.divider.setVisibility(View.VISIBLE);
+        }
+        else{
+            holder.title.setText(drawerMenuList.get(position - 1).getTitle());
+            holder.title.setVisibility(View.VISIBLE);
+            holder.divider.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return drawerMenuList.size()+1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        if(position == 0){
+            return  TYPE_HEADER;
+        }
+        else if(position == 5)
+        {
+            return TYPE_DIVIDER;
+        }
+        else {
+            return TYPE_MENU;
+        }
+
+    }
+
+    class DrawerViewHolder extends RecyclerView.ViewHolder{
+        TextView title;
+        TextView headerText;
+        View divider;
+
+
+        public DrawerViewHolder(View itemView, int viewType) {
+            super(itemView);
+
+
+            if(viewType == 0){
+
+            }
+
+                else
+             {
+                title = (TextView) itemView.findViewById(R.id.title);
+                 divider = (View) itemView.findViewById(R.id.divider);
+
+
+             }
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mListener.onItemSelected(view, getAdapterPosition());
+
+                }
+            });
+        }
+
+    }
+
+
+
+
+    public void setOnItemClickLister(OnItemSelecteListener mListener) {
+        this.mListener = mListener;
+    }
+
+   public interface OnItemSelecteListener{
+        public void onItemSelected(View v, int position);
+    }
+
+}
